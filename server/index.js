a180c86a2d138ead1683397f89b166d99aa9c130
brunoJorquera <jorquera.b18@gmail.com>
2021-09-23 14:54:34 -0700

    Lab endpoint

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+
+const app = express()
+app.use(express.static("client"))
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+const port = process.env.PORT || 6969
+app.listen(port, () => {
+    console.log(`Can you hear me on ${port}???` )
+})
